25ea75e5570d324ac91180ab21e64eccf8231d2b
DarkLabel <dark.laybel@bk.ru>
2020-02-07 20:50:20 +0300

    Version 1.1 (Comments)

--- Auto_ru.py
+++ Auto_ru.py
@@ -1,15 +1,14 @@
-import requests
-import os
-import time
+import requests #Библиотека для запроса
 
-
-a = 1
-while a <= 99:
+a = 1 #Переменная для перехода по страницам
+while a <= 99: #Всего 99 страниц на сайте
+    #Объявление переменных как глобальные
     global License_plate, Availability, Category, Color_hex, Description, Custom_cleared, Owners_number, PTS, VIN, Vin_resolution
     global Year, Price_rub, Price_eur, Price_usd, Salon, Seller, Region, Timezone, Mileage, Tip_auto, Count_doors, Class_auto
     global Name_auto, trunk_volume_min, Marka_info, Model_info, Ik_summary
-    URL = 'https://auto.ru/-/ajax/desktop/listing/'
+    URL = 'https://auto.ru/-/ajax/desktop/listing/' #URL на который будет отправлен запрос
 
+    #Параметры запроса
     PARAMS = {
          'catalog_filter' : [{"mark": "MERCEDES"}],
          'section': "all",
@@ -17,6 +16,7 @@ while a <= 99:
          'sort': "fresh_relevance_1-desc",
          'page': a
         }
+    #Заголовки страницы
     HEADERS = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -36,46 +36,58 @@ while a <= 99:
         'x-requested-with': 'fetch'
     }
 
-    response = requests.post(URL, json=PARAMS, headers=HEADERS)
-    data = response.json()['offers']
+    response = requests.post(URL, json=PARAMS, headers=HEADERS) #Делаем post запрос на url
+    data = response.json()['offers'] #Переменная data хранит полученные объявления
 
-    img_url = []
+    img_url = [] #Словарь в котором будут все картинки
 
-    i = 0
-    while i <= len(data) - 1:
+    i = 0 #Переменная для перехода по объявлениям
+    while i <= len(data) - 1: #len(data)-1 это количество пришедших объявлений
+
+        #Доступность объявления
         try: Availability = 'Доступность: ' + str(data[i]['availability'])
         except: License_plate = 'Not availability'
 
+        #Категория автомобиля
         try: Category = 'Категория: ' + str(data[i]['category'])
         except: Category = 'Not category'
 
-        try: Color_hex = 'Цвет: ' + str(data[i]['color_hex'])
+        #Цвет автомобиля (возвращается в формате hex)
+        try: Color_hex = 'Цвет: #' + str(data[i]['color_hex'])
         except: Color_hex = 'Not color'
 
+        #Описание автомобиля
         try: Description = 'Описание: ' + str(data[i]['description'])
         except: Description = 'Not description'
 
+        #Растаможен ли автомобиль (возвращает True или False)
         try: Custom_cleared = 'Таможня: ' + str(data[i]['documents']['custom_cleared'])
         except: Custom_cleared = 'Not custom cleared'
 
+        #Лицензия на автомобиль
         try: License_plate = 'Лицензия: ' + str(data[i]['documents']['license_plate'])
         except: License_plate = 'Not license plate '
 
+        #Колличество владельцев автомобиля
         try: Owners_number = 'Количество владельцев: ' + str(data[i]['documents']['owners_number'])
         except: Owners_number = 'The number of owners is not specified'
 
+        #PTS автомобиля
         try: PTS = 'PTS: ' + str(data[i]['documents']['pts'])
         except: PTS = 'Not PTS'
 
+        #VIN автомобиля
         try: VIN = 'VIN: ' + str(data[i]['documents']['vin'])
         except: VIN = 'Not VIN'
 
         try: Vin_resolution = 'Vin разрешение: ' + str(data[i]['documents']['vin_resolution'])
         except: Vin_resolution = 'Not vin resolution '
 
+        #Год выпуска автомобиля
         try: Year = 'Год: ' + str(data[i]['documents']['year'])
         except: Year = 'Not year'
 
+        #Цена в рублях, евро и долларах
         try: Price_rub = 'Рубли: ' + str(data[i]['price_info']['RUR']) + '₽'
         except: Price_rub = 'Not price rub'
 
@@ -85,62 +97,80 @@ while a <= 99:
         try: Price_usd = 'Доллар: ' + str(data[i]['price_info']['USD']) + '$'
         except: Price_usd = 'Not price usd'
 
+        #С салона ли машина или нет
         try: Salon = 'С салона: ' + str(data[i]['salon']['is_official'])
         except: Salon = 'Not salon'
 
+        #Координаты места нахождения машины (возвращается долгота и широта)
         try: Seller = 'Координаты: ' + str(data[i]['seller']['location']['coord']['latitude']) + ':' + str(data[i]['seller']['location']['coord']['longitude'])
         except: Seller = 'Not seller'
 
+        #Регион, в котором находится автомобиль
         try: Region = 'Регион: ' + str(data[i]['seller']['location']['region_info']['name'])
         except: Region = 'Not region'
 
+        #Временная зона в которой находится автомобиль
         try: Timezone = 'Временная зона: ' + str(data[i]['seller']['location']['timezone_info']['abbr'])
         except: Timezone = 'Not timezone'
 
+        #Пробег автомобиля
         try: Mileage = 'Пробег: ' + str(data[i]['state']['mileage'])
         except: Mileage = 'Not mileage'
 
+        #Картинки автомобиля
+        #Возвращается несколько фото, мы их добавляем в словарь img_url
         for img in data[i]['state']['image_urls']:
             img_url.append(img['sizes']['1200x900'])
 
+        #Тип автомобиля
         try: Tip_auto = 'Тип автомобиля: ' + str(data[i]['vehicle_info']['configuration']['body_type'])
         except: Tip_auto = 'Not tip auto'
 
+        #Количество дверей у автомобиля
         try: Count_doors = 'Колличество дверей: ' + str(data[i]['vehicle_info']['configuration']['doors_count'])
         except: Count_doors = 'Not count doors'
 
+        #Класс автомобиля
         try: Class_auto = 'Класс автомобиля: ' + str(data[i]['vehicle_info']['configuration']['auto_class'])
         except: Class_auto = 'Not class auto'
 
+        #Название автомобиля
         try: Name_auto = 'Имя автомобиля: ' + str(data[i]['vehicle_info']['configuration']['human_name'])
         except: Name_auto = 'Not name auto'
 
+        #Объем багажника автомобиля
         try: trunk_volume_min = 'Объем багажника: ' + str(data[i]['vehicle_info']['configuration']['trunk_volume_min'])
         except: trunk_volume_min = 'Not trunk volume min'
 
+        #Марка автомобиля
         try: Marka_info = 'Марка автомобиля: ' + str(data[i]['vehicle_info']['mark_info']['name'])
         except: Marka_info = 'Not marka info'
 
+        #Модель автомобиля
         try: Model_info = 'Модель автомобиля: ' + str(data[i]['vehicle_info']['model_info']['name'])
         except: Model_info = 'Not model info'
 
+        #Информация об автомобиле
         try: Ik_summary = 'Информация: ' + str(data[i]['lk_summary'])
         except: Ik_summary = 'Not ik summary'
 
-        link_img = ''
-        for link_img_0 in img_url:
+        link_img = '' #Переменная для ссылок
+        for link_img_0 in img_url: #Перебираем ссылки из словаря img_url, и записываем их в одну переменную текстом
             link_img += str(link_img_0) + '\n'
 
-
+        #Переменные для разделения записей
         text_razdelitely1 = '================================================================================================================================='
         text_razdelitely2 = '================================================================================================================================='
         text_razdelitely3 = '=================================================================================================================================\n'
+
+        #Переменная в которую всё записываем
         text = str(License_plate) + '\n' + str(Availability) + '\n' + str(Category) + '\n' + str(Color_hex) + '\n' + str(Description) + '\n' + str(Custom_cleared) + '\n' + str(Owners_number) + '\n' + str(PTS) + '\n' + str(VIN) + '\n' + str(Vin_resolution) + '\n' + str(Year) + '\n' + str(Price_rub) + '\n' + str(Price_eur) + '\n' + str(Price_usd) + '\n' + str(Salon) + '\n' + str(Seller) + '\n' + str(Region) + '\n' + str(Timezone) + '\n' + str(Mileage) + '\n' + str(Tip_auto) + '\n' + str(Count_doors) + '\n' + str(Class_auto) + '\n' + str(Name_auto) + '\n' + str(trunk_volume_min) + '\n' + str(Marka_info) + '\n' + str(Model_info) + '\n' + str(Ik_summary) + '\n' + text_razdelitely1 + '\n' + text_razdelitely2 + '\n' + text_razdelitely3
 
-        with open('Save_auto1.txt', 'a', encoding='UTF-8') as file:
-            file.write(text)
-        i += 1
-    print('Page: ' + str(a))
-    a += 1
+        #Записываем переменную в файл
+        with open('Save_auto1.txt', 'a', encoding='UTF-8') as file: #Открываем файл Save_auto1.txt (создаётся автоматически), на дозапись (ключ a)
+            file.write(text) #Записываем переменную
+        i += 1 #Увеличиваем переменную перехода по объявлениям на 1
+    print('Page: ' + str(a)) #Выводим сообщение, какая страница записалась
+    a += 1 #Увеличиваем переменную страницы сайта на 1
 
-print('Successfully')
+print('Successfully') #Выводим информацию об успешном выполнении
