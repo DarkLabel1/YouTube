2238a50f68fd5246196f48c3983190509716d04c
DarkLabel <dark.laybel@bk.ru>
2020-02-10 14:34:12 +0300

    Version 2 (Release)

--- Wildberries/main.py
+++ Wildberries/main.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup as bs
 import csv
 
 
+url = input('Веведите ссылку на то что надо парсить: ')
 page = 1
 while page <= 100:
-    URL = 'https://www.wildberries.ru/catalog/muzhchinam/odezhda?page=' + str(page)
+    URL = url + '?page=' + str(page)
     HEADERS = {
         'Accept': '*/*',
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
@@ -23,23 +24,23 @@ while page <= 100:
         div_tovar = data_tovar.find_all('div', attrs={'class': 'product-content-v1'})
         for divs_tovar in div_tovar:
             title = divs_tovar.find('span', attrs={'class': 'brand'}).text + divs_tovar.find('span', attrs={'class': 'name'}).text
-            price = divs_tovar.find('span', attrs={'class': 'final-cost'}).text
+            price = divs_tovar.find('span', attrs={'class': 'final-cost'}).text.split('\n')
             color = divs_tovar.find('span', attrs={'class': 'color'}).text
-            description = divs_tovar.find_all('p')
             PRODUCTS.append({
                 'title': title,
-                'price': price,
-                'color': color
+                'price': price[1],
+                'color': color,
+                'href': href
             })
 
-    with open('Save.csv', 'a', encoding='UTF-8', newline='') as file:
-        pen = csv.writer(file)
+    with open('F:\\Pycharm Projects\\YouTube\\Wildberries\\Save.csv', 'a', encoding='UTF-8', newline='') as file:
+        pen = csv.writer(file, delimiter=';')
         i = 0
         if i == 0:
-            pen.writerow(('Названия', 'Цена', 'Цвет'))
+            pen.writerow(('Названия', 'Цена', 'Цвет', 'Ссылка'))
             i += 1
         for PRODUCT in PRODUCTS:
-            pen.writerow((PRODUCT['title'], PRODUCT['price'], PRODUCT['color']))
+            pen.writerow((PRODUCT['title'], PRODUCT['price'], PRODUCT['color'], PRODUCT['href']))
     print('Successfully page: ' + str(page))
     page += 1
 print('Successfully')
